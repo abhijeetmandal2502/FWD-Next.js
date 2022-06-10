f7278c65107585ef5948dfe97d79c9226122b7fb
trishapakhi <vijaya.mistri@gmail.com>
2022-06-10 16:54:07 +0530

    create login form

--- fwd/pages/index.js
+++ fwd/pages/index.js
@@ -3,7 +3,16 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home = () => {
-  return <h1 className="text-5xl font-bold underline red">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline red">Hello world!</h1>
+      <div className=" my-8 mx-48 p-4 bg-white rounded-xl flex space-x-6">
+        <div>isdhgsdif</div>
+        <div>isdhgsdif</div>
+        <div>hjh</div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
